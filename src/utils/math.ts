e672d1920c6fbea236df7f3d4cce676c15e0b043
Angyeoung <realandreyb@gmail.com>
2024-10-11 19:48:47 -0700

    added math

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -13,6 +13,36 @@ class Vector3 {
 
     static get zero(): Vector3 { return new Vector3(0, 0, 0); }
 
-    
+    add(vector: Vector3): Vector3 {
+        return new Vector3(
+            this.x + vector.x,
+            this.y + vector.y,
+            this.z + vector.z
+        );
+    }
+
+    subtract(vector: Vector3): Vector3 {
+        return new Vector3(
+            this.x - vector.x,
+            this.y - vector.y,
+            this.z - vector.z
+        );
+    }
+
+    multiply(n: number): Vector3 {
+        return new Vector3(
+            this.x * n,
+            this.y * n,
+            this.z * n
+        );
+    }
+
+    divide(n: number): Vector3 {
+        return new Vector3(
+            this.x * n,
+            this.y * n,
+            this.z * n
+        );
+    }
 
 }

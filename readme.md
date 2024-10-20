8ac39c2d416dbefa94a4272409cc25a0dd9e5293
Angyeoung <Angyeoung@users.noreply.github.com>
2024-10-20 08:34:51 +0000

    deploy: 72a748f324bed17af8a3543b6d16023b34236fa7

--- readme.md
+++
@@ -1,8 +0,0 @@
-# WebGL Game Engine
-
-put word
-
-# todo
-
-ui with uploading/deleteing/file system stuff ye possibly merge `gl.program`
-with `programUniforms` into a single structure
--- tests/math_test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from '@std/assert';
-import { Matrix4, Vector3 } from '../src/utils/math.ts';
-
-const rand = (): number => Math.round(Math.random() * 100) - 50;
-
-Deno.test(function mathTest() {
-    console.log(Matrix4.identity());
-});
